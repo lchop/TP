f6016e865375537863f90fc08242e999e600aa7d
Louis CHOPOT <louis.chopot@atoptima.com>
2022-09-09 12:13:01 +0200

    more

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const dayjs = require('dayjs');
 const pug = require('pug');
 const http = require('http');
 
-const students = [
+const menuItems = [
+    { path: '/', title: 'Home', isActive: true },
+    { path: '/delete', title: 'Delete', isActive: false },
+];
+
+let students = [
     { name : "Sonia", birth : formatDate("2019-14-05")},
     { name : "Antoine", birth :formatDate("2000-12-05")},
     { name : "Alice", birth : formatDate("1990-14-09")},
@@ -25,16 +30,60 @@ server.on('request', (request,response) => {
             try {
                 const renderTemplate = pug.compileFile(`./view/template/layout.pug`,  { pretty: true });
                 const result = renderTemplate({
-                    title: 'Search Hacker News',
+                    title: 'Students list',
+                    students,
+                    menuItems
                     });
-                console.log(result);
                 response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
                 response.write(result);  
+
             } catch (error) {
                 console.log(error);
                 response.writeHead(404,{'Content-Type':'text/html;charset=utf-8'});
                 response.write('<h1>404 Not Found</h1>');
-            }                  
+            }
+            if(request.method === 'POST'){
+                let data = '';
+                request.on('data', chunk => {
+                    data+=chunk;
+                });
+                request.on('end', () => {
+                    let dataSpit = data.split("&");
+                    let name = dataSpit[0].split('=')[1];
+                    let birth = dataSpit[1].split('=')[1];
+                    students.push({name: name, birth:formatDate(birth)});
+                    response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+                    response.end();
+                });
+            }
+            break;
+        case '/delete':
+            try {
+                const renderTemplate = pug.compileFile(`./view/template/delete.pug`,  { pretty: true });
+                const result = renderTemplate({
+                    title: 'Delete student',
+                    students
+                    });
+                response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+                response.write(result);  
+
+            } catch (error) {
+                console.log(error);
+                response.writeHead(404,{'Content-Type':'text/html;charset=utf-8'});
+                response.write('<h1>404 Not Found</h1>');
+            }
+            if(request.method === 'POST'){
+                let data = '';
+                request.on('data', chunk => {
+                    data+=chunk;
+                });
+                request.on('end', () => {
+                    let name = data.split('=')[1];
+                    students = students.filter((item) => item.name != name);
+                    response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+                    response.end();
+                });
+            }
             break;
         default:
             response.writeHead(404,{'Content-Type':'text/html;charset=utf-8'});
